1717c7ef5c2dc6018d520d34ce8eca2c7f6b170c
karmasinh <hackgamesanimation@gmail.com>
2025-11-25 01:51:00 -0400

    base tienda

---
+++ inc/footer.php
@@ -0,0 +1,45 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contáctanos</h4>
+                <a href="#" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Por qué elegirnos</h4>
+                <p> 
+                    Productos seleccionados para Tarija y la región.</br>
+                    Precios competitivos y promociones locales.</br>
+                    Entrega en Tarija y alrededores.</br>
+                    Atención cercana y confiable.</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Dirección</h4>
+                <p style="color: #FFF">Tarija, Bolivia</p>
+                <p style="color: #FFF">Centro de Tarija</p>
+                <p style="font-size:20px" aria-hidden="true">  Horario: Lun–Sáb 9:00–19:00</p>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true">  Teléfono/WhatsApp: (+591) 00000000</p> <a href="#" target="_blank" style="color: #5bc0de">Envíanos un mensaje</a></br>
+                E-mail: <a href="#" target="_blank" style="color: #5bc0de"> &nbsp; contacto@lacaserita.com.bo</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">Tienda La Caserita Tarija &copy; <?php echo date('Y'); ?></h5>
+</footer>
